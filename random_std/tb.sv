c77eed417d8c377d28a29531d915eab677e92604
jnjn0022 <jnjn0022@gmail.com>
2023-08-16 21:17:00 +0800

    update

--- random_std/tb.sv
+++ random_std/tb.sv
@@ -2,33 +2,92 @@ import uvm_pkg::*;
 
 class pkt;
 
-    rand int count;
+    rand logic [7:0] class_rand_num_1_to_9 = 100;
+    logic [7:0] class_num_1_to_9 = 100;
 
     constraint c1 {
-        count >= 2;
-        count <= 9;
+        class_rand_num_1_to_9 >= 2;
+        class_rand_num_1_to_9 <= 9;
     }
 
-    function new(string name="thursday_seq");
-        if (std::randomize(count)) `uvm_info("",$psprintf(" cnt=%0d .....",count),UVM_LOW)
-        else $finish;
-        if (std::randomize(this.count)) `uvm_info("",$psprintf(" cnt=%0d .....",count),UVM_LOW)
-        else $finish;
-        //if (std::randomize(local::count)) `uvm_info("",$psprintf(" cnt=%0d .....",count),UVM_LOW)
-        //else $finish;
-        if (randomize(count)) `uvm_info("",$psprintf(" cnt=%0d .....",count),UVM_LOW)
-        else $finish;
-        if (this.randomize()) `uvm_info("",$psprintf(" cnt=%0d .....",count),UVM_LOW)
-        else $finish;
-    endfunction: new
+    function new (string name = "pkt");
+        class_rand_num_1_to_9 = 200;
+        class_num_1_to_9 = 200;
+    endfunction
+
+    function logic [7:0] get_num();
+        logic [7:0] scope_num = 100;
+
+        if (std::randomize(class_rand_num_1_to_9))
+            `uvm_info("",$psprintf("class: class_rand_num_1_to_9=%0d .....",class_rand_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+        if (std::randomize(this.class_rand_num_1_to_9))
+            `uvm_info("",$psprintf("class: class_rand_num_1_to_9=%0d .....",class_rand_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+        if (randomize(class_rand_num_1_to_9))
+            `uvm_info("",$psprintf("class: class_rand_num_1_to_9=%0d .....",class_rand_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+        if (this.randomize()) // unuseful for scope_num
+            `uvm_info("",$psprintf("class: class_rand_num_1_to_9=%0d .....",class_rand_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+
+        if (std::randomize(scope_num))
+            `uvm_info("",$psprintf("class: scope_num=%0d .....",scope_num),UVM_LOW)
+        else
+            $finish;
+        //illegal
+        //if (std::randomize(this.scope_num))
+        //    `uvm_info("",$psprintf("class: scope_num=%0d .....",scope_num),UVM_LOW)
+        //else
+        //    $finish;
+        //illegal
+        //if (randomize(scope_num))
+        //    `uvm_info("",$psprintf("class: scope_num=%0d .....",scope_num),UVM_LOW)
+        //else
+        //    $finish;
+        if (this.randomize()) // unuseful for scope_num
+            `uvm_info("",$psprintf("class: scope_num=%0d .....",scope_num),UVM_LOW)
+        else
+            $finish;
+
+        if (std::randomize(class_num_1_to_9))
+            `uvm_info("",$psprintf("class: class_num_1_to_9=%0d .....",class_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+        if (std::randomize(this.class_num_1_to_9))
+            `uvm_info("",$psprintf("class: class_num_1_to_9=%0d .....",class_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+        if (randomize(class_num_1_to_9))
+            `uvm_info("",$psprintf("class: class_num_1_to_9=%0d .....",class_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+        if (this.randomize()) // unuseful for scope_num
+            `uvm_info("",$psprintf("class: class_num_1_to_9=%0d .....",class_num_1_to_9),UVM_LOW)
+        else
+            $finish;
+    endfunction
 
 endclass
 
 module tb;
-    pkt p1;
+    pkt p;
 
     initial begin
-        p1 = new();
+        p = new();
+        p.get_num();
+        p.randomize();
+
+        `uvm_info("",$psprintf("initial: class_rand_num_1_to_9=%0d .....",p.class_rand_num_1_to_9),UVM_LOW)
+        //illegal
+        //`uvm_info("",$psprintf("initial: scope_num=%0d .....",p.scope_num),UVM_LOW)
+
+        // unuseful for class_num_1_to_9
+        `uvm_info("",$psprintf("initial: class_num_1_to_9=%0d .....",p.class_num_1_to_9),UVM_LOW)
     end
 endmodule
 
